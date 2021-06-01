06cd90944e63853fc0e1991f717b34d0c9359b77
Winston R. Milling <Winston@Milli.ng>
2021-06-01 13:09:30 -0500

    Update install script to install zgen

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+# Link vimrc to repo
 ln -s ~/.vim/vimrc ~/.vimrc
+
+# Install zgen
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+
+# Grab vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install all plugins
 vim +PluginInstall +qall
